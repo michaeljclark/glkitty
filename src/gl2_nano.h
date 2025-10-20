af3b8b04b35f127a598b955abfa7da853312e863
Michael Clark <michaeljclark@mac.com>
2025-10-20 13:22:40 +1300

    update gl2_nano.h header comment copyright dates

--- src/gl2_nano.h
+++ src/gl2_nano.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Michael Clark <michaeljclark@mac.com>
+ * Copyright (c) 2020-2025 Michael Clark <michaeljclark@mac.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
